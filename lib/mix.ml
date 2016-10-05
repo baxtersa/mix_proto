1609a82359f28b1f645bd8bb2cd6b498d275a7c5
Sam Baxter <baxtersa14@gmail.com>
2016-10-04 22:38:31 -0400

    Implement some cli functionality to start analysis under sym_eval or typechecker.

--- lib/mix.ml
+++ lib/mix.ml
@@ -1,11 +1,59 @@
 open Ast
 open Parser
+open Symbolic_ast
 open Symbolic_interp
+open Typecheck
 
-let _ =
-  let filename : string = Sys.argv.(1) in
+module Command = Core.Std.Command
+
+let symbolic (s:string) =
+  let filename : string = s in
   let program : exp = from_file filename in
   print_endline "Input:";
   pp_exp Format.std_formatter program;
   print_newline ();
-  failwith "MIX not implemented..."
+  let s, _ = sym_eval [] initial_state program in
+  let g = guard_of s in
+  print_endline "Output:";
+  pp_sym_exp Format.std_formatter g;
+  print_newline ()
+
+let typed (s:string) =
+  let filename : string = s in
+  let program : exp = from_file filename in
+  print_endline "Input:";
+  pp_exp Format.std_formatter program;
+  print_newline ();
+  let t = typecheck [] program in
+  print_endline "Output:";
+  pp_typ Format.std_formatter t;
+  print_newline ()
+
+let run_prog sym typ () =
+  match sym, typ with
+  | Some s, None ->
+     symbolic s
+  | None, Some s ->
+     typed s
+  | Some _, Some _ ->
+     failwith "Must specify only one of -sym or -typ to begin static analysis."
+  | _ ->
+     failwith "Please specify whether to begin static analysis under symbolic evaluator or type checker."
+
+let spec =
+  let open Command.Spec in
+  empty
+  +> flag "-sym" (optional string)
+    ~doc:"FILE Execute static analysis under symbolic evaluator on the given file."
+  +> flag "-typ" (optional string)
+    ~doc:"FILE Execute static analysis under type checker on the given file."
+
+let command =
+  Command.basic
+    ~summary:"Launch Supernet virtual compiler and SDN controller"
+    ~readme:(fun () -> "More detailed information")
+    spec
+    run_prog
+
+let () =
+  Command.run command
